dc820fb17e934caad1684fb638453e626115a6f1
wayne <a0917242247@gmail.com>
2020-02-07 16:08:27 +0800

    first commit

---
+++ library/imports/3f/3f050607-bb2f-4fd8-8267-bcd3a0c53d5e.js
@@ -0,0 +1,57 @@
+"use strict";
+cc._RF.push(module, '3f050YHuy9P2IJnvNOgxT1e', 'Star');
+// stripts/Star.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+  extends: cc.Component,
+
+  properties: {
+    pickRadius: 0
+  },
+
+  // LIFE-CYCLE CALLBACKS:
+
+  onLoad: function onLoad() {},
+  getPlayerDistance: function getPlayerDistance() {
+    // 根据 player 节点位置判断距离
+    var playerPos = this.game.player.getPosition();
+    // 根据两点位置计算两点之间距离
+    var dist = this.node.position.sub(playerPos).mag();
+    return dist;
+  },
+  onPicked: function onPicked() {
+    // 当星星被收集时，调用 Game 脚本中的接口，生成一个新的星星
+    this.game.spawnNewStar();
+    // 调用 Game 脚本的得分方法
+    this.game.gainScore();
+    // 然后销毁当前星星节点
+    this.node.destroy();
+  },
+  start: function start() {},
+  update: function update(dt) {
+    // 每帧判断和主角之间的距离是否小于收集距离
+    if (this.getPlayerDistance() < this.pickRadius) {
+      // 调用收集行为
+      this.onPicked();
+      return false;
+    }
+    // 根据 Game 脚本中的计时器更新星星的透明度
+    var opacityRatio = 1 - this.game.timer / this.game.starDuration;
+    var minOpacity = 50;
+    this.node.opacity = minOpacity + Math.floor(opacityRatio * (255 - minOpacity));
+  }
+});
+
+cc._RF.pop();
